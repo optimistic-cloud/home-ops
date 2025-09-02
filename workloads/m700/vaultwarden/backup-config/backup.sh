f2fb07cd6c48fd6a651ceb41f1be6bf169582c02
Max Wassiljew <x9zz@proton.me>
2025-09-02 20:58:32 +0000

    Add backup and restore scripts for vaultwarden with Justfile support and update environment configurations

--- workloads/m700/vaultwarden/backup-config/backup.sh
+++ workloads/m700/vaultwarden/backup-config/backup.sh
@@ -12,9 +12,7 @@ curl_cmd=curl -fsS -m 10 --retry 5
 backup_dir="/opt/vaultwarden"
 export_dir="/tmp/vaultwarden/export"
 
-ping_hc() {
-    ${curl_cmd} -o /dev/null "https://hc-ping.com/${HC_UUID}${1}?create=1" || true
-}
+ping_hc() { ${curl_cmd} -o /dev/null "https://hc-ping.com/${HC_UUID}${1}?create=1" || true; }
 
 cleanup() { rm -rf "$export_dir"; }
 trap cleanup EXIT
@@ -30,8 +28,8 @@ sqlite3 "$backup_dir/data/db.sqlite3" ".backup '$export_dir/db.sqlite3'"
 git_commit=$(git ls-remote https://github.com/optimistic-cloud/home-ops.git HEAD | cut -f1)
 restic_version=$(restic version | cut -d ' ' -f2)
 
-# loop over env files end execute restic
 for file in *.env; do
+  (
     set -a
     source "$file"
     
@@ -47,6 +45,7 @@ for file in *.env; do
     ${restic_cmd} check --read-data-subset 25%
 
     set +a
+  )
 done
 
 rm -rf "$export_dir"
