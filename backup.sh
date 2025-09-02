304d405970f4a16974a0fd3270809016923ab9c7
Max Wassiljew <x9zz@proton.me>
2025-09-02 20:26:23 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -24,16 +24,27 @@ ping_hc "/start"
 rm -rf "$export_dir" && mkdir -p -m 700 "$export_dir"
 sqlite3 "$backup_dir/data/db.sqlite3" ".backup '$export_dir/db.sqlite3'"
 
+git_commit=$(git ls-remote https://github.com/optimistic-cloud/home-ops.git HEAD | cut -f1)
+restic_version=$(restic version | cut -d ' ' -f2)
+
 # loop over env files end execute
-restic --verbose=0 --quiet backup \
-    --files-from ./include.txt \
-    --exclude-file ./exclude.txt \
-    --exclude-caches \
-    --one-file-system \
-    --tag app=vaultwarden \
-    --tag git_commit=`git ls-remote https://github.com/optimistic-cloud/home-ops.git HEAD | cut -f1` \
-    --tag restic_version=`restic version | cut -d ' ' -f2`
-restic --verbose=0 --quiet check --read-data-subset 25%
+for file in *.env; do
+    set -a
+    source "$file"
+    
+    restic --verbose=0 --quiet backup \
+      --files-from ./include.txt \
+      --exclude-file ./exclude.txt \
+      --exclude-caches \
+      --one-file-system \
+      --tag app=vaultwarden \
+      --tag git_commit=${git_commit} \
+      --tag restic_version=${restic_version}
+    
+    restic --verbose=0 --quiet check --read-data-subset 25%
+
+    set +a
+done
 
 rm -rf "$export_dir"
 
