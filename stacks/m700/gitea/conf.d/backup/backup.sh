7da072a06c7acd6ae3e5f17bcaa5e90afcc7ebbf
Max Wassiljew <x9zz@proton.me>
2025-09-03 11:21:23 +0000

    Add Gitea service configuration, backup scripts, and environment variables

---
+++ stacks/m700/gitea/conf.d/backup/backup.sh
@@ -0,0 +1,54 @@
+  #!/usr/bin/env sh
+  set -euo pipefail
+
+  # Acquire lockfile to prevent concurrent execution
+  lockfile="/tmp/gitea-backup.lock"
+  exec 200>"$lockfile"
+  flock -n 200 || { echo "Another backup is running. Exiting."; exit 1; }
+
+  restic_cmd="restic --verbose=0 --quiet"
+  curl_cmd="curl -fsS -m 10 --retry 5"
+
+  backup_dir="/opt/gitea"
+  export_dir="/tmp/gitea/export"
+
+  ping_hc() { ${curl_cmd} -o /dev/null "https://hc-ping.com/${HC_UUID}${1}?create=1" || true; }
+
+  cleanup() { rm -rf "$export_dir"; }
+  trap cleanup EXIT
+
+  error() { ping_hc "/fail"; }
+  trap error ERR
+
+  ping_hc "/start"
+
+  rm -rf "$export_dir" && mkdir -p -m 700 "$export_dir"
+  sqlite3 "$backup_dir/data/db.sqlite3" ".backup '$export_dir/db.sqlite3'"
+
+  git_commit=$(git ls-remote https://github.com/optimistic-cloud/home-ops.git HEAD | cut -f1)
+  restic_version=$(restic version | cut -d ' ' -f2)
+
+  for file in *.env; do
+    [ -f "$file" ] || continue
+    (
+      set -a
+      source "$file"
+      
+      ${restic_cmd} backup \
+        --files-from ./include.txt \
+        --exclude-file ./exclude.txt \
+        --exclude-caches \
+        --one-file-system \
+        --tag app=gitea \
+        --tag git_commit=${git_commit} \
+        --tag restic_version=${restic_version}
+      
+      ${restic_cmd} check --read-data-subset 100%
+
+      set +a
+    )
+  done
+
+  rm -rf "$export_dir"
+
+  ping_hc ""
