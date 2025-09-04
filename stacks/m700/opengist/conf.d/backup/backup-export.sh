c8b33b49cdfae65ab3847a677b3f2d288a6205d8
Max Wassiljew <x9zz@proton.me>
2025-09-04 09:36:35 +0000

    Refactor backup scripts and update crontab paths for consistency

--- stacks/m700/opengist/conf.d/backup/backup-export.sh
+++ stacks/m700/opengist/conf.d/backup/backup-export.sh
@@ -3,7 +3,7 @@ export_data() {
   # 2 param: export directory
   # 3 param: app name
   
-  source /opt/conf.d/backup/export-sqlite.sh
+  source /opt/conf.d/backup/scripts/export-sqlite.sh
 
-  export_sqlite "$1/data/$3.db" ".backup '$2/$3.db'"
+  export_sqlite "$1/data/$3.db" "$2/$3.db"
 }
--- stacks/m700/vaultwarden/conf.d/backup/backup-export.sh
+++ stacks/m700/vaultwarden/conf.d/backup/backup-export.sh
@@ -3,7 +3,7 @@ export_data() {
   # 2 param: export directory
   # 3 param: app name
   
-  source /opt/conf.d/backup/export-sqlite.sh
+  source /opt/conf.d/backup/scripts/export-sqlite.sh
 
   export_sqlite "$1/data/db.sqlite3" "$2/db.sqlite3"
 }
