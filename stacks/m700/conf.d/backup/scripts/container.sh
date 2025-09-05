e5d57b697d271530023443b41a5670db985870d6
Max Wassiljew <x9zz@proton.me>
2025-09-05 09:24:35 +0200

    Update container.sh

--- stacks/m700/conf.d/backup/scripts/container.sh
+++ stacks/m700/conf.d/backup/scripts/container.sh
@@ -1,11 +1,35 @@
+wait_for() {
+  local name=$1
+  local desired_state=$2
+  local retries=12
+
+  while (( retries > 0 )); do
+    state=$(curl -s --unix-socket /var/run/docker.sock "http://localhost/containers/$name/json" | jq -r '.State.Status')
+    if [[ "${state}" == "${desired_state}" ]]; then
+      break
+    fi
+
+    ((retries--))
+    if (( retries == 0 )); then
+       exit 3
+    fi
+
+    sleep 5
+  done
+}
+
 start_container() {
   local name=$1
 
   "${curl_cmd}" --unix-socket /var/run/docker.sock -X POST http://localhost/containers/"${name}"/start
+
+  wait_for ${name} "running"
 }
 
 stop_container() {
   local name=$1
 
   "${curl_cmd}" --unix-socket /var/run/docker.sock -X POST http://localhost/containers/"${name}"/stop
+
+  wait_for ${name} "exited"
 }
