88ad56064e459d0b1ce18c9439461987da8f2a48
Max Wassiljew <x9zz@proton.me>
2025-09-05 09:02:53 +0200

    Update backup-export.sh

--- stacks/m700/vaultwarden/conf.d/backup/backup-export.sh
+++ stacks/m700/vaultwarden/conf.d/backup/backup-export.sh
@@ -1,9 +1,45 @@
 export_data() {
   # 1 param: source directory
+  local source_dir=$1
   # 2 param: export directory
+  local export_dir=$2
   # 3 param: app name
-  
-  source /opt/conf.d/backup/scripts/export-sqlite.sh
+  local app=$3
 
-  export_sqlite "$1/appdata/db.sqlite3" "$2/db.sqlite3"
+
+  # Stop container
+  docker container stop "${app}"
+
+  # export sqlite database
+  source /opt/conf.d/backup/scripts/export-sqlite.sh
+  export_sqlite "${source_dir}/appdata/db.sqlite3" "${export_dir}/db.sqlite3"
+}
+
+pre_backup() {
+  # 1 param: source directory
+  local source_dir=$1
+  # 2 param: export directory
+  local export_dir=$2
+  # 3 param: app name
+  local app=$3
+
+
+  # Stop container
+  docker container stop "${app}"
+
+  # export sqlite database
+  source /opt/conf.d/backup/scripts/export-sqlite.sh
+  export_sqlite "${source_dir}/appdata/db.sqlite3" "${export_dir}/db.sqlite3"
+}
+
+post_backup() {
+  # 1 param: source directory
+  local source_dir=$1
+  # 2 param: export directory
+  local export_dir=$2
+  # 3 param: app name
+  local app=$3
+
+  # Start container
+  docker container start "${app}"
 }
