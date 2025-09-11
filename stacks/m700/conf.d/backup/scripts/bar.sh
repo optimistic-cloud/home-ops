59cece7f37b43cbabf8ccf3b7b2eb80be6775f5e
Max Wassiljew <x9zz@proton.me>
2025-09-11 12:22:47 +0000

    Refactor backup app provider script to use env command for environment variable loading

--- stacks/m700/conf.d/backup/scripts/bar.sh
+++ stacks/m700/conf.d/backup/scripts/bar.sh
@@ -3,25 +3,20 @@
 help:
     just --list
 
-# Run restic backup
-#backup app:
-#    nu {{app}}.nu
-
 [group('restic')]
 [doc('Backup application')]
 backup app provider:
-    #!/usr/bin/env sh
-    set -euox pipefail
     env $(cat .env.{{app}}.{{provider}} | xargs) nu {{app}}.nu
 
-# Run restic backup with debug logs
+[group('restic')]
+[doc('Backup application with debug logs')]
 backup-with-debug app $NU_LOG_LEVEL="debug":
-    nu {{app}}.nu
+    env $(cat .env.{{app}}.{{provider}} | xargs) nu {{app}}.nu
 
 # Restore latest restic snapshot
 restore app:
-    nu {{app}}.nu restore
+    env $(cat .env.{{app}}.{{provider}} | xargs) nu {{app}}.nu restore
 
 # Restore latest restic snapshot with debug logs
 restore-with-debug app $NU_LOG_LEVEL="debug":
-    nu {{app}}.nu restore
+    env $(cat .env.{{app}}.{{provider}} | xargs) nu {{app}}.nu restore
