b87cf2bbcabed5e0dd3fcf94c84ec537d413c6ec
Max Wassiljew <x9zz@proton.me>
2025-09-11 12:23:33 +0000

    Fix backup app provider script to correct command naming for debug and restore actions

--- stacks/m700/conf.d/backup/scripts/bar.sh
+++ stacks/m700/conf.d/backup/scripts/bar.sh
@@ -10,13 +10,13 @@ backup app provider:
 
 [group('restic')]
 [doc('Backup application with debug logs')]
-backup-with-debug app $NU_LOG_LEVEL="debug":
+backup-with-debug app provider $NU_LOG_LEVEL="debug":
     env $(cat .env.{{app}}.{{provider}} | xargs) nu {{app}}.nu
 
 # Restore latest restic snapshot
-restore app:
+restore app provider:
     env $(cat .env.{{app}}.{{provider}} | xargs) nu {{app}}.nu restore
 
 # Restore latest restic snapshot with debug logs
-restore-with-debug app $NU_LOG_LEVEL="debug":
+restore-with-debug app provider $NU_LOG_LEVEL="debug":
     env $(cat .env.{{app}}.{{provider}} | xargs) nu {{app}}.nu restore
