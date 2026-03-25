bfe4a5f0b05114d6ef0b6cc0dddbedbeee5467aa
Max Wassiljew <27824730+maxwassiljew@users.noreply.github.com>
2026-03-25 09:17:10 +0000

    Add pocket-id service and backup scripts with environment configuration

---
+++ stacks/m700/pocket-id/prepare_backup_data.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+set -eoux pipefail
+
+name="pocket-id"
+export_path="${EXPORT_DATA:?EXPORT_DATA is required}"
+
+cleanup() {
+  # remove dump from container
+  docker exec -u git "$name" rm -f "${dump_location}/${archive_name}"
+  # remove dump from export path
+  rm -f "${export_path}/${archive_name}"
+  # remove extracted dump from temp path
+  rm -rf "${tmp_dir}"
+}
+trap cleanup EXIT
+
+# export container.env 
+export_container_env() {
+  docker container inspect "$name" | jq -r '.[0].Config.Env[]' > "$export_path/$name.env"
+}
+
+# export sqlite database
+export_sqlite_db() {
+  local volume_name="$name-data"
+  local db_name="pocket-id.db"
+
+  docker run --rm -v "$volume_name":/data -v "$export_path":/export alpine/sqlite "/data/$db_name" ".backup '/export/$db_name'"
+  result=$(docker run --rm -v "$export_path":/export alpine/sqlite /export/$db_name 'PRAGMA integrity_check;')
+  if [ "$result" = "ok" ]; then
+    echo "Database is valid"
+  else
+    echo "Database is NOT valid"
+    exit 1
+  fi
+
+  docker run --rm -v "$export_path":/export alpine/sqlite /export/$db_name ".tables"
+}
+
+export_encfile() {
+  local volume_name="$name-data"
+  local tmp_dir=$(mktemp -d)
+
+  docker cp "$name":/app/secrets/pocket-id.encfile" "$tmp_dir"
+  docker run --rm -v "$tmp_dir":/data -v "$export_path":/export alpine cp -r /data/* /export
+}
+
+export_container_env
+export_sqlite_db
+export_encfile
