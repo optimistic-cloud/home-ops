3dc6f3223bc95c2899205853e6675fb61b18c164
Max Wassiljew <27824730+maxwassiljew@users.noreply.github.com>
2026-03-25 09:27:39 +0000

    Fix export command in encfile backup to ensure correct file copying

--- stacks/m700/pocket-id/prepare_backup_data.sh
+++ stacks/m700/pocket-id/prepare_backup_data.sh
@@ -31,7 +31,7 @@ export_encfile() {
   local tmp_dir=$(mktemp -d)
 
   docker cp "$name":/app/secrets/pocket-id.encfile "$tmp_dir"
-  docker run --rm -v "$tmp_dir":/data -v "$export_path":/export alpine cp -r /data/* /export
+  docker run --rm -v "$tmp_dir":/data -v "$export_path":/export alpine cp -r /data/ /export
 }
 
 export_container_env
