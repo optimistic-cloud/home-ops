6620b3f0a2acc426dd3135bf140645b47249331c
Max Wassiljew <27824730+maxwassiljew@users.noreply.github.com>
2026-04-08 16:46:19 +0000

    Refactor backup process by moving initialization logic to backup.sh and commenting out inline implementation in Justfile

--- stacks/m700/vaultwarden/backup.sh
+++ stacks/m700/vaultwarden/backup.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 
+hc_api="${HC_API:?HC_API is required}"
+hc_ping_key="${HC_PING_KEY:?HC_PING_KEY is required}"
+
+run_id=$(cat /proc/sys/kernel/random/uuid)
+hc_check_base_name=vaultwarden
+hc_base_url="https://${hc_api}/ping/${hc_ping_key}/${hc_check_base_name}"
+curl_cmd="curl -fsS -m 10 --retry 5 -o /dev/null"
+
+# ping start
+for backup_target in {{backup_targets}}; do
+${curl_cmd} "${hc_base_url}-${backup_target}/start?create=1&rid=${run_id}"
+done
+
+bash prepare_backup_data.sh
+
 for backup_target in "$@"; do
   on_error() {
     ${curl_cmd} "${hc_base_url}-${backup_target}/fail?rid=${run_id}"
