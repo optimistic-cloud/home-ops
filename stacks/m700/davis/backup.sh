8a8f72d01d97a9b13a92c4578571badb4cba9ca1
Max Wassiljew <27824730+maxwassiljew@users.noreply.github.com>
2026-04-24 19:19:54 +0000

    Fix environment file reference in backup script for restic forget command

--- stacks/m700/davis/backup.sh
+++ stacks/m700/davis/backup.sh
@@ -169,7 +169,7 @@ for backup_target in "${EXEC_BACKUP_TARGETS[@]}"; do
   #   -v /mnt/data/m700/davis:/repo \
   #   "${restic_image}" \
   #   forget --keep-within 365d --quiet)"
-  output="$(RESTIC_ENV_FILE="${target}.restic.env" docker compose -f docker-compose.backup.yaml run --rm restic forget --keep-within 365d --quiet --json | jq)"
+  output="$(RESTIC_ENV_FILE="${backup_target}.restic.env" docker compose -f docker-compose.backup.yaml run --rm restic forget --keep-within 365d --quiet --json | jq)"
 
   exit_code=$?
   ping_result "${backup_target}" "${exit_code}" "${output}"
